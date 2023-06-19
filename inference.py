bc6556a300a1d06fc820af40ed4815d04c42f87a
Karanja <98053458+thirtyninetythree@users.noreply.github.com>
2023-06-19 21:34:39 +0300

    initial commit

---
+++ inference.py
@@ -0,0 +1,123 @@
+from diffusers import (
+    StableDiffusionPipeline,
+    StableDiffusionControlNetImg2ImgPipeline,
+    ControlNetModel,
+    DDIMScheduler,
+    DPMSolverMultistepScheduler,
+    DEISMultistepScheduler,
+    HeunDiscreteScheduler,
+    EulerDiscreteScheduler,
+)
+
+import torch
+from PIL import Image
+import qrcode
+from pathlib import Path
+from multiprocessing import cpu_count
+import requests
+import io
+import os
+from PIL import Image
+
+SAMPLER_MAP = {
+    "DPM++ Karras SDE": lambda config: DPMSolverMultistepScheduler.from_config(config, use_karras=True, algorithm_type="sde-dpmsolver++"),
+    "DPM++ Karras": lambda config: DPMSolverMultistepScheduler.from_config(config, use_karras=True),
+    "Heun": lambda config: HeunDiscreteScheduler.from_config(config),
+    "Euler": lambda config: EulerDiscreteScheduler.from_config(config),
+    "DDIM": lambda config: DDIMScheduler.from_config(config),
+    "DEIS": lambda config: DEISMultistepScheduler.from_config(config),
+}
+
+qrcode_generator = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+controlnet = ControlNetModel.from_pretrained(
+    "DionTimmer/controlnet_qrcode-control_v1p_sd15", torch_dtype=torch.float16
+)
+
+pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    controlnet=controlnet,
+    safety_checker=None,
+    torch_dtype=torch.float16,
+).to("cuda")
+pipe.enable_xformers_memory_efficient_attention()
+
+def resize_for_condition_image(input_image: Image.Image, resolution: int):
+    input_image = input_image.convert("RGB")
+    W, H = input_image.size
+    k = float(resolution) / min(H, W)
+    H *= k
+    W *= k
+    H = int(round(H / 64.0)) * 64
+    W = int(round(W / 64.0)) * 64
+    img = input_image.resize((W, H), resample=Image.LANCZOS)
+    return img
+
+
+def inference(
+    qr_code_content: str,
+    prompt: str,
+    negative_prompt: str,
+    guidance_scale: float = 10.0,
+    controlnet_conditioning_scale: float = 2.0,
+    strength: float = 0.8,
+    seed: int = -1,
+    init_image: Image.Image | None = None,
+    qrcode_image: Image.Image | None = None,
+    use_qr_code_as_init_image = True,
+    sampler = "DPM++ Karras SDE",
+):
+    if prompt is None or prompt == "":
+      print("Prompt is required")
+      return
+        # raise gr.Error("Prompt is required")
+
+    if qrcode_image is None and qr_code_content == "":
+      print("QR Code Image or QR Code Content is required")
+      return
+        # raise gr.Error("QR Code Image or QR Code Content is required")
+
+    pipe.scheduler = SAMPLER_MAP[sampler](pipe.scheduler.config)
+
+    generator = torch.manual_seed(seed) if seed != -1 else torch.Generator()
+
+    if qr_code_content != "" or qrcode_image.size == (1, 1):
+        print("Generating QR Code from content")
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(qr_code_content)
+        qr.make(fit=True)
+
+        qrcode_image = qr.make_image(fill_color="black", back_color="white")
+        qrcode_image = resize_for_condition_image(qrcode_image, 768)
+    else:
+        print("Using QR Code Image")
+        qrcode_image = resize_for_condition_image(qrcode_image, 768)
+
+    # hack due to gradio examples
+    init_image = qrcode_image
+
+    out = pipe(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        image=qrcode_image,
+        control_image=qrcode_image,  # type: ignore
+        width=768,  # type: ignore
+        height=768,  # type: ignore
+        guidance_scale=float(guidance_scale),
+        controlnet_conditioning_scale=float(controlnet_conditioning_scale),  # type: ignore
+        generator=generator,
+        strength=float(strength),
+        num_inference_steps=40,
+    )
+    return out.images[0]  # type: ignore
+   
